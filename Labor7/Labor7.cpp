3fee50e0845ef0f9f92d5a94e7a86a01cece4c74
AndriiSmolinskyi <awful.braid@gmail.com>
2023-01-03 15:41:39 +0200

    Labor7
    Labor7

---
+++ Labor7/Labor7.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int result;
+void aver(int a, int b, int c);
+
+
+int main()
+{
+    int a, b, c;
+
+    cin >> a;
+    cin >> b;
+    cin >> c;
+
+    aver(a, b, c);
+
+
+}
+
+void aver(int a, int b, int c)
+{
+    result = (a + b + c) / 3;
+    cout << result;
+
+}
